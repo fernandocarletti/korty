91d0e06bcc01ccecd968b9a3ff7e003f6691eee8
xurumelous <xurumelous@gmail.com>
2010-11-29 18:12:52 -0200

    * Form tag. Usage: 	{form <same html atrributes>} 	{form action='controller/action' id='form'} 	{form action=['controller'=>'controller_name', 'action'=>'action_name'] id="form_id"} 	{form action='http://sample.com' method='post'}

---
+++ plugins/block.form.php
@@ -0,0 +1,90 @@
+<?php 
+/**
+ * Gernerate form tag.
+ * 
+ * @param array $params
+ * @param string $content
+ * @param string $template
+ * @param string $repeat
+ */
+function smarty_block_form($params, $content, $template, &$repeat)
+{
+	// Auto id for forms. (I've no idea why it's incrementing 2 times per execution.
+	static $ids;
+	
+	// Get the request instance. Here we can get the controller and the action in use.
+	$request = Request::instance();
+	
+	// The form attributes will be here.
+	$attributes = '';
+	
+	// If user set the action param...
+	if(isset($params['action']))
+	{
+		// If it's and array, it should have two associative indexes: controller and action
+		if(is_array($params['action']))
+		{
+			// If controller is not set, we use the current.
+			if(isset($params['action']['controller'])) 
+			{
+				$controller = $params['action']['controller'];				
+			}
+			else
+			{
+				$controller = $request->controller;
+			}
+			
+			// If action is not set, we use the current.
+			if(isset($params['action']['action']))
+			{
+				$action = $params['action']['action'];
+			}
+			else
+			{
+				$action = $request->action;
+			}
+			
+			// Set the target url.
+			$target = url::site($controller.'/'.$action); 
+		}
+		
+		// If the action param starts with http, it should be an url
+		elseif(preg_match('/^http/D', $params['action']))
+		{
+			$target = $params['action'];
+		}
+		
+		// Else, it shoud be <controller>/<action>/... format
+		else
+		{
+			$target = url::site($params['action']);
+		}
+		
+		// Set the action attribute.
+		$attributes .= 'action="'.$target.'" ';
+	}
+	
+	// Setting the auto id
+	if(!isset($params['id']))
+	{
+		$attributes .= 'id="form_'.($ids++).'" ';
+	}
+	
+	foreach($params as $key => $value)
+	{
+		if(strtolower($key) == 'action')
+		{
+			continue;
+		}
+		
+		$attributes .= $key.'="'.$value.'" ';
+	}
+	
+	$form = "<form {$attributes}>"
+	      . $content
+	      . "</form>";
+	
+	return $form;
+}
+
+?>
