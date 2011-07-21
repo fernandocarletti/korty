01fb6fe142957ea1e3c35f5cd33bf6c02f8859ec
xurumelous <xurumelous@gmail.com>
2011-07-21 15:34:43 -0300

    Added support for less dynamic stylesheet language

--- plugins/function.stylesheet.php
+++ plugins/function.stylesheet.php
@@ -1,18 +1,23 @@
 <?php 
 
 function smarty_function_stylesheet($params, $template) {
-	$stylesheet_tag = "<link rel=\"stylesheet\"";
+	$stylesheet_tag = '<link rel="stylesheet"';
+
+	if($params['less'])
+	{
+		$stylesheet_tag = '<link rel="stylesheet/less"';
+	}
 		
 	foreach($params as $key => $value) {
-		if(strtolower($key) == "src") {
-			$src = URL::base()."public/stylesheets/".$params["src"];
-			$stylesheet_tag .= " href=\"{$src}\" ";
+		if(strtolower($key) == 'src') {
+			$src = URL::base().'public/stylesheets/'.$params['src'];
+			$stylesheet_tag .= ' href="{$src}" ';
 		} else {
-			$stylesheet_tag .= " {$key}=\"{$value}\" ";
+			$stylesheet_tag .= ' {$key}="{$value}" ';
 		}
 	}
 	
-	$stylesheet_tag .= "/>";
+	$stylesheet_tag .= '/>';
 	
 	return $stylesheet_tag;
 }
