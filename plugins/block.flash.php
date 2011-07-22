2e94e4e4c6c201656b816473f5f787d34322cdd5
xurumelous <xurumelous@gmail.com>
2011-07-22 11:49:00 -0300

    Fixing a bug while deleting flash session var.

--- plugins/block.flash.php
+++ plugins/block.flash.php
@@ -15,7 +15,11 @@ function smarty_block_flash($params, $content, $template, &$repeat)
 	if( ! is_null($session->get("_korty_flash_{$params['id']}")))
 	{
 		$str = str_ireplace('#message#', $session->get("_korty_flash_{$params['id']}"), $content);
-		$session->delete("flash_{$params['id']}");
+
+		if( ! $repeat)
+		{
+			$session->delete("_korty_flash_{$params['id']}");
+		}
 	}
 
 	return $str;
